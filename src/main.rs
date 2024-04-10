cfaf44d89312c1ccddee379027e883aae6de5c5c
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-04-10 21:32:08 +0200

    reorganize device setup & firmware loading

--- src/main.rs
+++ src/main.rs
@@ -52,31 +52,34 @@ fn command(device: &rusb::DeviceHandle<rusb::GlobalContext>, cmd: &[u32]) {
         .unwrap();
 }
 
+fn setup_device() -> Option<rusb::DeviceHandle<rusb::GlobalContext>> {
+    let mut device =
+        rusb::open_device_with_vid_pid(VENDOR_MICROSOFT, PRODUCT_K4W_AUDIO_ORIGINAL).unwrap();
+
+    if device.active_configuration().unwrap() != KINECT_AUDIO_CONFIGURATION {
+        device
+            .set_active_configuration(KINECT_AUDIO_CONFIGURATION)
+            .unwrap();
+    }
+
+    device.set_auto_detach_kernel_driver(true).unwrap();
+
+    device.claim_interface(KINECT_AUDIO_INTERFACE).unwrap();
+
+    if device.active_configuration().unwrap() != KINECT_AUDIO_CONFIGURATION {
+        println!("device configuration changed!");
+        return None;
+    }
+    Some(device)
+}
+
 fn main() {
     let filename = std::env::args()
         .nth(1)
         .unwrap_or("firmware.bin".to_string());
 
-    let device = {
-        let mut device =
-            rusb::open_device_with_vid_pid(VENDOR_MICROSOFT, PRODUCT_K4W_AUDIO_ORIGINAL).unwrap();
-
-        if device.active_configuration().unwrap() != KINECT_AUDIO_CONFIGURATION {
-            device
-                .set_active_configuration(KINECT_AUDIO_CONFIGURATION)
-                .unwrap();
-        }
-
-        device.set_auto_detach_kernel_driver(true).unwrap();
-
-        device.claim_interface(KINECT_AUDIO_INTERFACE).unwrap();
-
-        if device.active_configuration().unwrap() != KINECT_AUDIO_CONFIGURATION {
-            println!("device configuration changed!");
-            return;
-        }
-        device
-    };
+    let firmware: Vec<u8> = std::fs::read(filename).expect("Failed to open file");
+    let device = setup_device().unwrap();
 
     let mut seq = 1u32;
 
@@ -96,8 +99,6 @@ fn main() {
 
     // read status
     assert!(status(&device, status_cmd, seq));
-
-    let firmware: Vec<u8> = std::fs::read(filename).expect("Failed to open file");
 
     const PAGESIZE: usize = 0x4000;
     let pages = firmware.chunks(PAGESIZE);
