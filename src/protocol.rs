591a036def39ed2c1743f465beafabdbc7549280
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-04-24 23:11:07 +0200

    merge test modules

--- src/protocol.rs
+++ src/protocol.rs
@@ -43,92 +43,6 @@ mod internal {
         }
     }
 
-    pub mod command {
-
-        #[cfg(test)]
-        mod tests {
-            use super::super::Command;
-
-            fn le_bytes(cmd: &[u32; 6]) -> Vec<u8> {
-                let cmd_buffer = cmd
-                    .iter()
-                    .map(|arg| arg.to_le_bytes())
-                    .collect::<Vec<[u8; 4]>>()
-                    .concat();
-                cmd_buffer
-            }
-
-            #[test]
-            fn generate_status_command() {
-                let seq = 1;
-                let status_cmd = [
-                    0x06022009u32, // magic
-                    seq,           // tag
-                    0x00000060u32, // payload size
-                    0x00000000u32, // command = version
-                    0x00000015u32, // address
-                    0x00000000u32, // unk
-                ];
-                let status = Command {
-                    tag: seq,
-                    command: 0,
-                    address: 0x15,
-                    size: 0x60,
-                    unk: 0,
-                };
-
-                assert_eq!(le_bytes(&status_cmd), status.bytes());
-            }
-
-            #[test]
-            fn generate_page_command() {
-                let seq = 7;
-                let page_len: usize = 0x10_000;
-                let address = 0x80_000;
-                let page_cmd = [
-                    0x06022009u32,     // magic
-                    seq,               // tag
-                    (page_len as u32), // payload size
-                    0x00000003u32,     // command = write page
-                    address,           // address
-                    0x00000000u32,     // unk
-                ];
-                let page = Command {
-                    tag: seq,
-                    command: 3,
-                    address,
-                    size: page_len as u32,
-                    unk: 0,
-                };
-
-                assert_eq!(le_bytes(&page_cmd), page.bytes());
-            }
-
-            #[test]
-            fn generate_finished_command() {
-                let seq = 11;
-                let entry_point = 0x80_030;
-                let finished_cmd = [
-                    0x0602_2009u32, // magic
-                    seq,            // tag
-                    0x0000_0000u32, // payload size
-                    0x0000_0004u32, // command = finish upload
-                    entry_point,    // address
-                    0x0000_0000u32, // unk
-                ];
-                let finished = Command {
-                    tag: seq,
-                    command: 0x04,
-                    address: entry_point,
-                    size: 0,
-                    unk: 0,
-                };
-
-                assert_eq!(le_bytes(&finished_cmd), finished.bytes());
-            }
-        }
-    }
-
     pub fn receive_status(
         device: &rusb::DeviceHandle<rusb::GlobalContext>,
     ) -> Result<Status, Error> {
@@ -157,6 +71,86 @@ mod internal {
 
     #[cfg(test)]
     mod tests {
+        use super::Command;
+
+        fn le_bytes(cmd: &[u32; 6]) -> Vec<u8> {
+            let cmd_buffer = cmd
+                .iter()
+                .map(|arg| arg.to_le_bytes())
+                .collect::<Vec<[u8; 4]>>()
+                .concat();
+            cmd_buffer
+        }
+
+        #[test]
+        fn generate_status_command() {
+            let seq = 1;
+            let status_cmd = [
+                0x06022009u32, // magic
+                seq,           // tag
+                0x00000060u32, // payload size
+                0x00000000u32, // command = version
+                0x00000015u32, // address
+                0x00000000u32, // unk
+            ];
+            let status = Command {
+                tag: seq,
+                command: 0,
+                address: 0x15,
+                size: 0x60,
+                unk: 0,
+            };
+
+            assert_eq!(le_bytes(&status_cmd), status.bytes());
+        }
+
+        #[test]
+        fn generate_page_command() {
+            let seq = 7;
+            let page_len: usize = 0x10_000;
+            let address = 0x80_000;
+            let page_cmd = [
+                0x06022009u32,     // magic
+                seq,               // tag
+                (page_len as u32), // payload size
+                0x00000003u32,     // command = write page
+                address,           // address
+                0x00000000u32,     // unk
+            ];
+            let page = Command {
+                tag: seq,
+                command: 3,
+                address,
+                size: page_len as u32,
+                unk: 0,
+            };
+
+            assert_eq!(le_bytes(&page_cmd), page.bytes());
+        }
+
+        #[test]
+        fn generate_finished_command() {
+            let seq = 11;
+            let entry_point = 0x80_030;
+            let finished_cmd = [
+                0x0602_2009u32, // magic
+                seq,            // tag
+                0x0000_0000u32, // payload size
+                0x0000_0004u32, // command = finish upload
+                entry_point,    // address
+                0x0000_0000u32, // unk
+            ];
+            let finished = Command {
+                tag: seq,
+                command: 0x04,
+                address: entry_point,
+                size: 0,
+                unk: 0,
+            };
+
+            assert_eq!(le_bytes(&finished_cmd), finished.bytes());
+        }
+
         use super::{Response, Status};
 
         #[test]
