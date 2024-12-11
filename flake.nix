c464d317ef195d080a464777cd02683babba8e26
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-12-11 17:51:50 +0100

    call the wix-extract flake output directly when extracting firmware blob

--- flake.nix
+++ flake.nix
@@ -40,10 +40,10 @@
               url = "https://download.microsoft.com/download/E/C/5/EC50686B-82F4-4DBF-A922-980183B214E6/KinectRuntime-v1.8-Setup.exe";
               hash = "sha256-9NQUP7DwqNJ2iJwHe/yK9Cv+mcEoytq14xa/AVqYWOk=";
             };
-            buildInputs = [wix-extract.packages.${system}.default p7zip];
+            buildInputs = [p7zip];
 
             unpackPhase = ''
-              wix-extract $src -d $TMP
+              ${wix-extract.apps.${system}.default.program} $src -d $TMP
               7z e -y -r $TMP/KinectDrivers-v1.8-x86.WHQL.msi "UACFirmware" > /dev/null
             '';
 
